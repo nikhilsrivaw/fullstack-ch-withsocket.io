b07c5b7b5b6061a23a5883f3d152534b2d228d17
nikhilsrivaw <nikhilksrivastav190@gmail.com>
2025-03-27 23:23:37 +0530

    half done

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,6 @@
 import {create} from "zustand"
 import { axiosInstance } from "../lib/axios";
-import { signup } from "../../../backend/src/controllers/auth.controller";
+
 
 export const useAuthStore = create((set)=>({
     authUser : null,
